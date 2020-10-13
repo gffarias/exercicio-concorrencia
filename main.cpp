be85c3683ee0ff730ad8a6ea1cc3b6cee49e93f3
Gustavo F. de Farias <gff@cin.ufpe.br>
2020-10-12 23:16:19 -0300

    Aplicado uso de threads com mutex e variavel condicional

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ class Song {
     std::string artist;
     std::string album;
     std::string title;
-    int duration;     // em segundos
+    int duration;
 public:
     Song (
         const std::string& artist,
@@ -21,20 +21,91 @@ public:
         title(title),
         duration(duration)
     {}
+    // acessa a duração da música (em segundos)
+    int getDuration () {
+        return duration;
+    }
+    /* operator overloading que define a relação
+     * de igualdade entre dois objetos Song */
+    bool operator== (const Song& o) const {
+        return artist == o.artist && album == o.album && title == o.title;
+    }
+    friend std::ostream& operator<< (std::ostream& os, const Song& o) {
+        // TODO elaborar representação de objeto Song como string
+        return os << o.title << std::endl;
+    }
 };
 
 std::list<Song> playlist;
+std::list<Song>::iterator currentlyPlaying;
+bool userQuitted;
 
-// inserir uma música na playlist
-void addSong () {
-    system("clear");
-    std::cout << "Aqui você escolhe qual música você quer adicionar" << std::endl;
+pthread_mutex_t mutex;
+pthread_cond_t emptyPlaylist;
+
+void addSong (const Song& song, int pos) {
+    pthread_mutex_lock(&mutex);
+    playlist.insert(std::next(playlist.begin(), pos), song);
+    pthread_cond_signal(&emptyPlaylist);
+    pthread_mutex_unlock(&mutex);
 }
 
-// remover uma música da playlist
-void removeSong () {
-    system("clear");
-    std::cout << "Aqui você escolhe qual música você quer remover" << std::endl;
+void removeSong (int pos) {
+    pthread_mutex_lock(&mutex);
+    playlist.erase(std::next(playlist.begin(), pos));
+    pthread_mutex_unlock(&mutex);
+}
+
+bool playlistContainsSong (const Song& song) {
+    for (const auto& it : playlist) {
+        if (it == song) {
+            return true;
+        }
+    }
+    return false;
+}
+
+void quit () {
+    pthread_mutex_lock(&mutex);
+    userQuitted = true;
+    pthread_mutex_unlock(&mutex);
+}
+
+Song getCurrentSong () {
+    pthread_mutex_lock(&mutex);
+    if (playlist.empty()) {
+        // TODO programar uma user-defined exception apropriada
+        pthread_mutex_unlock(&mutex);
+        throw -1;
+    } else {
+        Song song = *currentlyPlaying;
+        pthread_mutex_unlock(&mutex);
+        return song;
+    }
+}
+
+void* play (void* args) {
+    while (true) {
+        pthread_mutex_lock(&mutex);
+        if (userQuitted) {
+            pthread_mutex_unlock(&mutex);
+            break;
+        } else if (playlist.empty()) {
+            pthread_cond_wait(&emptyPlaylist, &mutex);
+            pthread_mutex_unlock(&mutex);
+        } else {
+            // comportamento cíclico
+            if (currentlyPlaying == playlist.end()) {
+                currentlyPlaying = playlist.begin();
+            } else {
+                currentlyPlaying++;
+            }
+            int duration = currentlyPlaying->getDuration();
+            pthread_mutex_unlock(&mutex);
+            sleep(duration);
+        }
+    }
+    pthread_exit(NULL);
 }
 
 int main () {
@@ -46,44 +117,62 @@ int main () {
         Song("Pink Floyd", "Wish You Were Here", "Wish You Were Here", 317),
         Song("Elis Regina & Tom Jobim", "Elis & Tom", "Águas de Março", 212)
     };
-    // flag que controla se deve permanecer no menu principal ou não
+    // inicializando explicitamente todas as variáveis globais
+    pthread_mutex_init(&mutex, NULL);
+    pthread_cond_init(&emptyPlaylist, NULL);
+    userQuitted = false;
+    currentlyPlaying = playlist.begin();
+
+    // variáveis locais
+    pthread_t playlistLoop;
+    pthread_create(&playlistLoop, NULL, play, NULL);
     bool keep;
+
     do {
-        // imprimir opções de caracteres a serem digitados pelo usuário
-        std::cout << "Olá, bem-vindo ao Music Player!" << std::endl;
-        std::cout << "Opções..." << std::endl;
+        // TODO imprimir opções de caracteres a serem digitados pelo usuário
+        std::cout << "--opções--" << std::endl;
+
         // Lê o caractere que o usuário digitou
         char input;
         std::cin >> input;
 
         switch (input) {
             case '+': {
-                addSong();
-                sleep(2);
+                // TODO interação com usuário para inserção de música
+                int i;
+                std::cin >> i;
+                addSong(database[i], 0);
                 keep = true;
             } break;
             case '-': {
-                removeSong();
-                sleep(2);
+                // TODO interação com usuário para remoção de música
+                int i;
+                std::cin >> i;
+                removeSong(i);
                 keep = true;
             } break;
-            case 'q': { // sair do menu
-                std::cout << "Flw!" << std::endl;
-                sleep(2);
+            case '?': {
+                try {
+                    std::cout << getCurrentSong() << std::endl;
+                } catch (...) {
+                    std::cout << "A playlist está vazia" << std::endl;
+                }
+                keep = true;
+            } break;
+            case '.': {
+                quit();
+                std::cout << "O ciclo da playlist foi encerrado." << std::endl;
+                std::cout << "Espere até a última música acabar." << std::endl;
                 keep = false;
             } break;
-            case 'c': { // consultar música em execução
-                std::cout << "A musica em reprodução é: " << std::endl;
-                sleep(2);
-                break;
-            }
             default: {
-                std::cout << "Entrada inválida, tente novamente" << std::endl;
-                sleep(2);
+                std::cout << "Entrada inválida" << std::endl;
                 keep = true;
             }
         }
-
-        system("clear");   // apaga tudo para recomeçar
     } while (keep);
+    pthread_join(playlistLoop, NULL);
+    // destruindo
+    pthread_mutex_destroy(&mutex);
+    pthread_cond_destroy(&emptyPlaylist);
 }
